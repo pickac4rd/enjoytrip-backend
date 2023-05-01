5d40e57d2f35283911def27dc34555dbc3699840
변정원 <labhg64@gmail.com>
2023-05-01 13:55:15 +0900

    first commit

---
+++ src/main/java/com/TripTestApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripTestApplication.class, args);
+    }
+
+}
