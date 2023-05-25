ca4e993f523d6fac6384ee619acc79355f6b6e1e
sgkim <kimsg64090@gmail.com>
2023-05-25 21:18:01 +0900

    feat 리뷰 아이디 추가

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/board")
 @Api("게시판 컨트롤러  API V1")
